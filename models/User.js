7fbe32e56bd471ec7e182831bc79fda9a2d49029
Eric Furukawa <eric.furukawa@gmail.com>
2022-01-12 12:48:47 -0800

    created Post model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-const { update } = require('lodash');
 
 //create User model
 class User extends Model {
